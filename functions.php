34731fef37b9a123505f563fceaaeb6be79a3782
Paul Rosen <paul@nines.org>
2011-12-15 17:53:36 -0500

    Added readme file; removed old commented code.

--- functions.php
+++ functions.php
@@ -1,20 +1,4 @@
 <?php
-//*************************************
-// Use the primary sidebar for post type pages and
-// the secondary sidebar for about type pages.
-//function disable_blog_primary($sidebars_widgets) {
-//	if ( ! is_admin() ) {
-//		if ( is_page() ){
-//			$sidebars_widgets['primary'] = false;
-//		} else {
-//			$sidebars_widgets['secondary'] = false;
-//		}
-//	}
-//
-//	return $sidebars_widgets;
-//}
-//
-//add_filter( 'sidebars_widgets', 'disable_blog_primary');
 
 //*************************************
 // Change the breadcrumbs to be hidden for pages and be formatted differently
